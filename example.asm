3eabd8feff777532400ad6521bc59b6b72dca47b
pat <patrickcaulfield1177@gmail.com>
2023-05-21 19:06:29 +0100

    x

--- example.asm
+++ example.asm
@@ -1,115 +1,90 @@
 JUMP start
 
-x: 32 y: 32
 k: 0
+x: 0 y: 0
+moving: 0
 
-dec_x:
-	CMP x 0
-	ELSE @end
-		SET x 70
-	@end:
-		DEC x
-		RET
-dec_y:
-	CMP y 0
-	ELSE @end
-		SET y 70
-	@end:
-		DEC y
-		RET
-inc_x:
-	CMP x 68
-	ELSE @end
-		SET x 0
-	@end:
-		INC x
-		RET
-inc_y:
-	CMP y 68
-	ELSE @end
-		SET y 0
-	@end:
-		INC y
-		RET
+ptr: 0 ptr_: 0
+char: 0
+string: W E L C O M E SPACE T O SPACE F E A B ! ? ! ? 0
 
-update:
-	SET 8 x ; sprite 0, x
-	SET 9 y ; sprite 0, y
+print_string:
+	@loop:
+		DEREF char ptr
+		CHAR char
+		INC ptr_
+		CMP char 0
+		ELSE @loop
+	CHAR 10
 	RET
 
+# CMPLEFT 	GET k KEYS AND k LEFT CMP k 0 #
+# CMPRIGHT 	GET k KEYS AND k RIGHT CMP k 0 #
+# CMPUP	 	GET k KEYS AND k UP CMP k 0 #
+# CMPDOWN	GET k KEYS AND k DOWN CMP k 0 #
+
 start:
-	SET 2 %000000 ; color 0
-	SET 3 %110000 ; color 1
-	SET 4 %001100 ; color 2
-	SET 5 %000011 ; color 3
-	OR 0 4 ; load sprite
-	SET 6 ff ; enable sprites
+	REF ptr string
+	CALL print_string
 
-	SET 8 32 ; sprite 0, x
-	SET 9 32 ; sprite 0, y
-	SET 10 %10000010 ; sprite 0, row 0
-	SET 11 %11101011 ; sprite 0, row 1
-	SET 12 %10010110 ; sprite 0, row 2
-	SET 13 %10101010 ; sprite 0, row 3
+	SET PALETTE_1 %110000
+	SET PALETTE_2 %001100
+	SET PALETTE_3 %000011
 
-	; F
-	SET 14 24 ; sprite 1, x
-	SET 15 24 ; sprite 1, y
-	SET 16 %10100000 ; sprite 1, row 0
-	SET 17 %10000000 ; sprite 1, row 1
-	SET 18 %10100000 ; sprite 1, row 2
-	SET 19 %10000000 ; sprite 1, row 3
+	GET SPRITE_0_S s
 
-	; E
-	SET 20 27 ; sprite 2, x
-	SET 21 24 ; sprite 2, y
-	SET 22 %10101000 ; sprite 2, row 0
-	SET 23 %10101000 ; sprite 2, row 1
-	SET 24 %10000000 ; sprite 2, row 2
-	SET 25 %10101000 ; sprite 2, row 3
+	SET x 32
+	SET y 32
+	GET SPRITE_0_X x
+	GET SPRITE_0_Y y
+	SET SPRITE_0_ROW_0 %10000010
+	SET SPRITE_0_ROW_1 %11101011
+	SET SPRITE_0_ROW_2 %10010110
+	SET SPRITE_0_ROW_3 %10101010
 
-	; A
-	SET 26 31 ; sprite 3, x
-	SET 27 24 ; sprite 3, y
-	SET 28 %00000000 ; sprite 3, row 0
-	SET 29 %10101000 ; sprite 3, row 1
-	SET 30 %10001000 ; sprite 3, row 2
-	SET 31 %10101010 ; sprite 3, row 3
+	SET SPRITE_1_ROW_0 %00001010
+	SET SPRITE_1_ROW_1 %00101110
+	SET SPRITE_1_ROW_2 %00100101
+	SET SPRITE_1_ROW_3 %00101010
 
-	; B
-	SET 32 36 ; sprite 4, x
-	SET 33 24 ; sprite 4, y
-	SET 34 %10000000 ; sprite 4, row 0
-	SET 35 %10101000 ; sprite 4, row 1
-	SET 36 %10001000 ; sprite 4, row 2
-	SET 37 %10101000 ; sprite 4, row 3
+	SET SPRITE_2_ROW_0 %10100000
+	SET SPRITE_2_ROW_1 %10111000
+	SET SPRITE_2_ROW_2 %01011000
+	SET SPRITE_2_ROW_3 %10101000
 
-loop:
-	@left:
-		SET k &1
-		AND k 1
-		CMP k 0
-		IF @right
-		CALL dec_x
-	@right:
-		SET k &1
-		AND k 2
-		CMP k 0
-		IF @up
-		CALL inc_x
-	@up:
-		SET k &1
-		AND k 4
-		CMP k 0
-		IF @down
-		CALL dec_y
-	@down:
-		SET k &1
-		AND k 8
-		CMP k 0
-		IF @done
-		CALL inc_y
-	@done:
-		CALL update
-		WAIT
-		JUMP loop
+	OR FLAGS 4 ; load sprite
+
+	@loop:
+		SET moving 0
+		@left:
+			CMPLEFT IF @right
+				DEC x
+				SET moving 1
+				SET SPRITE_0_S 2
+				SET PALETTE_0 %101010
+		@right:
+			CMPRIGHT IF @up
+				INC x
+				SET moving 1
+				SET SPRITE_0_S 1
+				SET PALETTE_0 %110011
+		@up:
+			CMPUP IF @down
+				DEC y
+				SET moving 1
+				SET PALETTE_0 %101011
+		@down:
+			CMPDOWN IF @move
+				INC y
+				SET moving 1
+				SET PALETTE_0 %111001
+		@move:
+			CMP moving 0
+			ELSE @done
+				SET SPRITE_0_S 0
+				SET PALETTE_0 0
+		@done:
+			GET SPRITE_0_X x
+			GET SPRITE_0_Y y
+			WAIT
+			JUMP @loop
